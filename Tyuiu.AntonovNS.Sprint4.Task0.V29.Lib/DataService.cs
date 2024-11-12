88dab95d937442a911dd03b98ed69af4b83a48d2
Никита Антонов <Tyuiu.AntonovNS@outlook.com>
2024-11-12 17:47:19 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.AntonovNS.Sprint4.Task0.V29.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces;
+using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.AntonovNS.Sprint4.Task0.V29.Lib
+{
+    public class DataService : ISprint4Task0V29
+    {
+        public int GetSumOddArrEl(int[] array)
+        {
+            int res = 0;
+            for (int i = 1; i <= array.Length; i++) 
+            {
+                if (i % 2 != 0)
+                    res += array[i];
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.AntonovNS.Sprint4.Task0.V29.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.AntonovNS.Sprint4.Task0.V29.Lib;
+namespace Tyuiu.AntonovNS.Sprint4.Task0.V29.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumOddArrEl()
+        {
+            DataService ds = new DataService();
+            int[] numsArray = { 7, 4, 3, 2, 1, 5, 9, 8, 7, 4 };
+            int wait = 23;
+            int res = ds.GetSumOddArrEl(numsArray);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
