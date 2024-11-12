27a201a3ad6c1d7f0050aef368de207ae8671016
Никита Антонов <Tyuiu.AntonovNS@outlook.com>
2024-11-12 18:00:18 +0500

    Task1

--- Tyuiu.AntonovNS.Sprint4.Task1.V21.Lib/DataService.cs
+++ Tyuiu.AntonovNS.Sprint4.Task1.V21.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.AntonovNS.Sprint4.Task1.V21.Lib
     {
         public int Calculate(int[] array)
         {
-            int res = 0;
+            int res = 1;
             for (int i = 0; i <= array.Length - 1; i++)
             {
                 if (array[i] % 2 == 0)
