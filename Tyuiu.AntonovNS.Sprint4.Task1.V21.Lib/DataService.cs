cd9dcfa85c4efd55e21b54e581050d6ff4628203
Никита Антонов <Tyuiu.AntonovNS@outlook.com>
2024-11-12 18:00:03 +0500

    Task1

--- Tyuiu.AntonovNS.Sprint4.Task1.V21.Lib/DataService.cs
+++ Tyuiu.AntonovNS.Sprint4.Task1.V21.Lib/DataService.cs
@@ -10,7 +10,7 @@ namespace Tyuiu.AntonovNS.Sprint4.Task1.V21.Lib
             for (int i = 0; i <= array.Length - 1; i++)
             {
                 if (array[i] % 2 == 0)
-                    res += array[i];
+                    res *= array[i];
             }
             return res;
         }
